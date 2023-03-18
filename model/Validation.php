2cab41ca7db72d14cc143d14beb64100cf91a118
Jack Zeng <zeng.zhenhuai@student.greenriver.edu>
2023-03-17 20:08:20 -0400

    validation and amdmin 1 modified

--- model/Validation.php
+++ model/Validation.php
@@ -2,9 +2,49 @@
 
 class Validation
 {
-    static function validPet($pet)
+
+    /**
+     * validName() checks to see that a string is all alphabetic or non-empty.
+     * @return boolean
+     */
+    static function validName($firstName)
     {
-        //will search the checkboxes in Adoption page to verify selections match
-        return in_array($pet, DataLayer::getPets());
+        if (preg_match("/\D\w/", $firstName)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    /**
+     * validName() checks to see that a string is all alphabetic or non-empty.
+     * @return boolean
+     */
+    static function validLastName($lastName)
+    {
+        if (preg_match("/\D\w/", $lastName)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * validPhone() checks to see that a phone number is valid (you can decide what constitutes a valid phone number, just make sure to check that there are numeric values entered).
+     * @return boolean
+     */
+    static function validPhone($phoneNumber)
+    {
+        return preg_match("/\d{3}-\d{3}-\d{4}/", $phoneNumber);
+    }
+
+    /**
+     * validEmail() checks to see that an email address is valid.
+     * @return boolean
+     */
+    static function validEmail($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 }
